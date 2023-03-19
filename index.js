28bccc9e8c0d6a47746aee774377fd0c104c933b
Olesya Mikhaylova <olesyamikhaylova@Air-Olesya.station>
2023-03-19 15:57:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log(40000 + 22000 + ' рублей');
+
+console.log('Владивосток ' + '2000');
+
+console.log((128 + 254 + 137 + 201) / 60 + ' часов');
+
+console.log(2 ** 5);
+
+console.log('"Нужно' + ' бежать' + ' со' + ' всех' + ' ног' + ' ,' + ' чтобы' + ' только' + ' оставаться' + ' на' + ' месте' + ' ,' + ' а' + ' чтобы' + ' куда-то' + ' попасть' + ' ,' + ' надо' + ' бежать' + ' как' + ' минимум' + ' вдвое' + ' быстрее' + ' !"');
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ', ' +  name + '!');
+
+let time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+    time -= 15;
+    console.log('Немного сторис в соцсетях. Осталось минут: ' + time); 
+    time -= 10;
+    console.log('Немного не новостей, но событий. Осталось минут: ' + time);  
+    time = 0;
+    console.log('Вы приехали. Добро пожаловать в Москву.'); 
+ 
+ let fahrenheit = 451;
+ let celsius= (fahrenheit - 32) / 1.8;
+ fahrenheit = _fahrenheit_ =451;
+ celsius = _celsius_ = (fahrenheit - 32) / 1.8;
+ console.log(_fahrenheit_  + 'градуса по Фаренгейту — это ' + _celsius_ + 'градуса по Цельсию.');
+ 
